6ca88200bf1341429fc2c8587c3d220b4a462eb9
min0962 <wsx8028@gmail.com>
2025-05-07 12:28:18 +0900

    feat: 결재단계 엔티티, enum 추가

---
+++ backend/team6/src/main/java/programmers/team6/domain/vacation/entity/ApprovalStep.java
@@ -0,0 +1,39 @@
+package programmers.team6.domain.vacation.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import programmers.team6.domain.member.entity.Member;
+import programmers.team6.domain.vacation.enums.ApprovalStatus;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ApprovalStep {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "vacation_request_id", nullable = false)
+	private VacationRequest vacationRequest;
+
+	private int step;
+
+	@Column(name = "approval_status", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private ApprovalStatus status;
+
+}
