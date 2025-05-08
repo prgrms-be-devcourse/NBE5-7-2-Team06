c83db84d1de39112158d823d8d478b7992e42794
rhwlgns4386 <30650239+rhwlgns4386@users.noreply.github.com>
2025-05-08 16:15:53 +0900

    Feat(11) : 휴가현황 조회 기능 구현 (#17)
    * feat : 사용자별 통계관련 sql작성 및 페이지 적용
    
    * docs : 추가적인 주석 작성
    
    * chore : 정렬 수정
    
    * docs : 추가적인 주석 추가
    
    * refactor : 통계 명칭 변경
    
    조금더 의미에 맞는 단어로 summery -> statistics로 변경 하였습니다.
    
    * feat : 추가적인 쿼리 튜닝
    
    limt절을 그룹바이로 옮겨서 join범위를 줄였습니다.

--- backend/team6/src/main/java/programmers/team6/domain/vacation/entity/VacationRequest.java
+++ backend/team6/src/main/java/programmers/team6/domain/vacation/entity/VacationRequest.java
@@ -2,18 +2,11 @@ package programmers.team6.domain.vacation.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import programmers.team6.domain.member.entity.Code;
+import programmers.team6.domain.member.entity.Member;
 import programmers.team6.global.entity.BaseEntity;
 
 @Entity
@@ -41,6 +34,9 @@ public class VacationRequest extends BaseEntity {
 	@JoinColumn(name = "status_code", nullable = false)
 	private Code status;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
 	@Version
 	private Integer version;
 }
