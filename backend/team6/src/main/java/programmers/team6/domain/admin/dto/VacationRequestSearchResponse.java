dfda9072c8fa4af999b7116cbfd894fc6f129a45
Gunwoong cho <80460636+gunwoong1630@users.noreply.github.com>
2025-05-12 16:43:50 +0900

    feat(1) : 관리자 신청 휴가 조회 및 필터링 검색 서비스 구현  (#23)
    * feat: ApprovalStatus 객체 추가 및 VacationRequest 필드 수정
    
    1. ApprovalStatus 엔티티화
    2. VacationRequest
    - from과 to의 LocalDateTime => LocalDate (기간 비교 단순화를 위해)
    - ApprovalStatus와 결제 정보 동기화를 위해 VacationRequest에 2개의 함수 추
    
    * refactor: 이후 테스트를 위해 cascade 추가 및 repository 부재 추가
    
    * feat: 관리자 측면 휴가 신청 조회 및 필터링 검색
    
    Repository에서 Query로 진행하는 과정에서 복잡한 구조에 비해 확장성이 떨어져서 Repository를 따로 생성하고 Criteria를 활용하여 필터링 구현
    
    필터링 내역
    * 1. 휴가 신청 범위
    * 2. 특정 년도 혹은 특정 분기 (1,2,3,4,상,하반기)
    * 3. 휴가 신청자 이름
    * 4. 부서 이름
    * 5. 휴가 종류
    * 6. 휴가 신청자 포지션
    * 7. 휴가 신청 상태
    * 8. 휴가 결재자이름
    
    1. AdminVacationSearchCondition setter 제거
    2. enum 디렉토리 생성 및 이동, Quarter IsoField 활용하여 구현
    3. criteria 수정
    - 기존 방식(문자열 기반 검색)에서 메타모델 활용 방식으로 전환, 그리하여 문자열 기반의 방식의 런타임 과정에서 에러 발생 가능성을 낮춤
    - hibernate-jpamodelgen gradle 추가하여 메타 모델을 자동 생성으로 전환
    - criteria를 활용한 필터링 쿼리 코드 수정
        - 각 기능(필터링, 쿼리 생성, 쿼리 수행, 페이징)을 분리하여 필터링과 쿼리 생성은 빌더를 활용하여 리팩토링 수행
        - Provide the parametrized type for this generic. 문제 해결, generic 구체화를 위한 기본 타입 설명 아래 참고
    4. record 적극 도입
    - 불변 이슈와 편의성을 위해 기존 클래스에서 record로 전환
    
    From<A,B> = 'from 엔티티'에서 엔티티 부분 , A타입의 객체부터 B타입의 속성을 탐색 (그래서 Root<A,A>이고 Join<A,B>임, Root와 Join 둘다 From 상속)
    SingularAttribute<C,D> = 메타모델에서 사용하는 단일 속성 타입 정보 , C = 특정 엔티티, D = C 엔티티의 필드 타입
    Path<X> = 메타모델 경로 혹은 루트로부터 탐색된 속성(특정 속성의 경로) , X = 속성 타입, 해당 path가 가르키는 최종 필드 타입
    
    * feat: criteriautil 추가
    
    연관관계가 있는 클래스들을 Root에서 연달아 찾아가야할 경우 위와같이 Path를 찾아내는 기능 추가
    
    * feat: 최신순 정렬 추가
    
    생성 순으로 휴가 신청 조회기능 추
    
    * feat: VacationRequest 상세 조회 및 수정
    
    * refactor: 제네릭 누락 및 record 사용 시 배열 사용 부분 리팩토링
    
    1. 제네릭이 누락될경우 컴파일러가 해당 클래스를 raw type으로 두고 타입 추론을 해주지 않습니다. => 제네릭은 항상 명시
    
    2. record는 기본적으로 get,hashCode,equal,toString 메서드가 있는데 원소에 배열이 있을 경우 예상치 못한 결과 발생 가능 => 배열을 List로 수정
    
    * refactor: controller url/api 수정
    
    * refactor: Quarter 관련 리팩토링
    
    Quarter에서 null check하는 것은 월권, DTO부분이나 빌더부분에서 NULL 처리
    
    * refactor: Quarter 메서드명 변경
    
    * refactor: dev merge 전 최종 점검
    
    * feat: dept repository 추가
    
    * refactor: erd cloud 기반 엔티티 순서 정렬

---
+++ backend/team6/src/main/java/programmers/team6/domain/admin/dto/VacationRequestSearchResponse.java
@@ -0,0 +1,15 @@
+package programmers.team6.domain.admin.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import programmers.team6.domain.vacation.enums.VacationRequestStatus;
+
+public record VacationRequestSearchResponse(String type, LocalDate from, LocalDate to, String applicantName,
+											List<String> approverNames, String deptName, VacationRequestStatus status) {
+	public VacationRequestSearchResponse(String type, LocalDate from, LocalDate to, String applicantName,
+		String approverNames, String deptName, VacationRequestStatus status) {
+		this(type, from, to, applicantName, List.of(approverNames.split(",")), deptName, status);
+	}
+
+}
