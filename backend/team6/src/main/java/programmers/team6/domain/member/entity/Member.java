57215fa93aeed05f11683312c2b76c58f35d80e0
min0962 <wsx8028@gmail.com>
2025-05-14 09:13:35 +0900

    fix: 충돌 해결

--- backend/team6/src/main/java/programmers/team6/domain/member/entity/Member.java
+++ backend/team6/src/main/java/programmers/team6/domain/member/entity/Member.java
@@ -22,9 +22,9 @@ import lombok.Setter;
 import programmers.team6.domain.member.enums.Role;
 import programmers.team6.global.entity.BaseEntity;
 
+@Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
 public class Member extends BaseEntity {
 
 	@Id
