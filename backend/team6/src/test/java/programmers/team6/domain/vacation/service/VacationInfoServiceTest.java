cacc36ecf81ff4a6dcac5e0194fbd50d9e8aa2d7
hunko <rhwlgns4386@naver.com>
2025-05-14 16:11:56 +0900

    test : 호출하는 리파지토리 수정에 따른 mock 수정

--- backend/team6/src/test/java/programmers/team6/domain/vacation/service/VacationInfoServiceTest.java
+++ backend/team6/src/test/java/programmers/team6/domain/vacation/service/VacationInfoServiceTest.java
@@ -16,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import programmers.team6.domain.vacation.dto.VacationInfoUpdateTotalCountRequest;
 import programmers.team6.domain.vacation.dto.VacationInfoUpdateTotalCountRequests;
+import programmers.team6.domain.vacation.dto.VacationInfoUpdateTotalCountRequestsList;
 import programmers.team6.domain.vacation.entity.VacationInfo;
 import programmers.team6.domain.vacation.enums.VacationInfoUpdateResult;
 import programmers.team6.domain.vacation.repository.VacationInfoRepository;
@@ -67,17 +68,14 @@ class VacationInfoServiceTest {
 		VacationInfoUpdateTotalCountRequest vacationInfoUpdateTotalCountRequest2 = createVacationDto(2, 13, "test2", 0);
 		VacationInfoUpdateTotalCountRequests request = createUpdateTotalCountRequest(
 			vacationInfoUpdateTotalCountRequest1, vacationInfoUpdateTotalCountRequest2);
-		VacationInfo vacationInfo1 = createVacationInfo(vacationInfoUpdateTotalCountRequest1,
-			VacationInfoUpdateResult.SUCCESS);
-		VacationInfo vacationInfo2 = createVacationInfo(vacationInfoUpdateTotalCountRequest2,
-			VacationInfoUpdateResult.SUCCESS);
-		long memberId = 1L;
-		when(repository.findAllByMemberId(memberId)).thenReturn(List.of(vacationInfo1, vacationInfo2));
+		VacationInfo vacationInfo1 = createVacationInfo(vacationInfoUpdateTotalCountRequest1, VacationInfoUpdateResult.SUCCESS);
+		VacationInfo vacationInfo2 = createVacationInfo(vacationInfoUpdateTotalCountRequest2, VacationInfoUpdateResult.SUCCESS);
+		when(repository.findAllByVacationIdIn(List.of(1,2))).thenReturn(List.of(vacationInfo1, vacationInfo2));
 
-		service.updateFrom(memberId, request);
+		service.updateFrom(new VacationInfoUpdateTotalCountRequestsList(List.of(request)));
 
 		verify(vacationInfo1, times(1)).updateTotalCount(vacationInfoUpdateTotalCountRequest1.version(), 12);
-		verify(vacationInfo2, times(1)).updateTotalCount(vacationInfoUpdateTotalCountRequest1.version(), 13);
+		verify(vacationInfo2, times(1)).updateTotalCount(vacationInfoUpdateTotalCountRequest2.version(), 13);
 	}
 
 	private VacationInfoUpdateTotalCountRequest createVacationDto(int id, int totalCount, String type, int version) {
@@ -86,7 +84,7 @@ class VacationInfoServiceTest {
 
 	private VacationInfoUpdateTotalCountRequests createUpdateTotalCountRequest(
 		VacationInfoUpdateTotalCountRequest... vacationInfoUpdateTotalCountRequests) {
-		return new VacationInfoUpdateTotalCountRequests(List.of(vacationInfoUpdateTotalCountRequests));
+		return new VacationInfoUpdateTotalCountRequests(1L,List.of(vacationInfoUpdateTotalCountRequests));
 	}
 
 	private VacationInfo createVacationInfo(VacationInfoUpdateTotalCountRequest infoDto,
@@ -94,6 +92,7 @@ class VacationInfoServiceTest {
 		VacationInfo vacationInfo = mock(VacationInfo.class);
 		when(vacationInfo.updateTotalCount(infoDto.version(), infoDto.totalCount())).thenReturn(result);
 		when(vacationInfo.getVacationType()).thenReturn(infoDto.type());
+		when(vacationInfo.getMemberId()).thenReturn(1L);
 		return vacationInfo;
 	}
 
