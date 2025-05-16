db7e13c433b153bc85c6c1cac4ee206bebcf7fe1
hunko <rhwlgns4386@naver.com>
2025-05-16 15:26:45 +0900

    feat : 기본 레이아웃 업로드

--- front/team06/src/App.js
+++ front/team06/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import AdminLayout from './pages/component/admin-layout';
+import Vacations from "./pages/component/vacations/vacations";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <AdminLayout>
+          <Routes>
+              <Route path="/vacations" element={<Vacations />} />
+          </Routes>
+        </AdminLayout>
+      </Router>
   );
 }
 
