bb097dfaac5b641c86fce5758b77a65b82851061
gunwoong <gwj0421@gmail.com>
2025-05-16 15:50:58 +0900

    feat: 관리자 휴가계 조회 및 코드 관리를 위한 기본 데이터 추가
    프론트에서 사용할 데이터들(대분류코드 종류 등)을 프론트에 전달하고 기존 검색 필터에 결재자 이름 제

--- backend/team6/src/main/java/programmers/team6/domain/admin/controller/AdminController.java
+++ backend/team6/src/main/java/programmers/team6/domain/admin/controller/AdminController.java
@@ -1,11 +1,11 @@
 package programmers.team6.domain.admin.controller;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import programmers.team6.domain.admin.dto.AdminVacationRequestSearchResponse;
 import programmers.team6.domain.admin.dto.AdminVacationSearchCondition;
 import programmers.team6.domain.admin.dto.VacationRequestDetailReadResponse;
 import programmers.team6.domain.admin.dto.VacationRequestDetailUpdateRequest;
-import programmers.team6.domain.admin.dto.VacationRequestSearchResponse;
 import programmers.team6.domain.admin.service.AdminService;
 
 @RestController
@@ -29,9 +29,9 @@ public class AdminController {
 
 	@GetMapping("/vacation-request")
 	@ResponseStatus(HttpStatus.OK)
-	Page<VacationRequestSearchResponse> selectVacationRequests(
+	AdminVacationRequestSearchResponse selectVacationRequests(
 		@PageableDefault(page = 0, size = 20, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-		@RequestBody @Valid AdminVacationSearchCondition searchCondition) {
+		@ModelAttribute @Valid AdminVacationSearchCondition searchCondition) {
 		return adminService.search(pageable, searchCondition);
 	}
 
--- backend/team6/src/main/java/programmers/team6/domain/member/repository/CodeRepository.java
+++ backend/team6/src/main/java/programmers/team6/domain/member/repository/CodeRepository.java
@@ -7,7 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import programmers.team6.domain.admin.dto.CodeInfo;
 import programmers.team6.domain.member.dto.CodeReadResponse;
 import programmers.team6.domain.member.entity.Code;
 
@@ -19,7 +21,13 @@ public interface CodeRepository extends JpaRepository<Code, Long> {
 	List<Code> findByGroupCode(String vacationType);
 
 	@Query(value = "select new programmers.team6.domain.member.dto.CodeReadResponse(c.id,c.groupCode,c.code,c.name) "
-		+ "from Code c")
-	Page<CodeReadResponse> findCodePage(Pageable pageable);
+		+ "from Code c where (:groupCode is null or c.groupCode = :groupCode)")
+	Page<CodeReadResponse> findCodePage(Pageable pageable, @Param("groupCode") String groupCode);
+
+	@Query(value = "select new programmers.team6.domain.admin.dto.CodeInfo(c.id,c.name) from Code c where c.groupCode = :groupCode")
+	List<CodeInfo> findCodeInfosByGroupCode(@Param("groupCode") String groupCode);
+
+	@Query(value = "select c.groupCode from Code c group by c.groupCode")
+	List<String> findGroupCodes();
 
 }
