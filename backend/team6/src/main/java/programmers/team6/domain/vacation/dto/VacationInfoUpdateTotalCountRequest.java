72331e5b030e1d6bd9eb85d78093a4130a26a1e9
hunko <rhwlgns4386@naver.com>
2025-05-19 00:50:25 +0900

    feat : 검증로직 수정

--- backend/team6/src/main/java/programmers/team6/domain/vacation/dto/VacationInfoUpdateTotalCountRequest.java
+++ backend/team6/src/main/java/programmers/team6/domain/vacation/dto/VacationInfoUpdateTotalCountRequest.java
@@ -1,10 +1,10 @@
 package programmers.team6.domain.vacation.dto;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
-public record VacationInfoUpdateTotalCountRequest(@NotNull Integer id, @NotNull @Positive Integer totalCount,
-												  @NotNull String type, @NotNull Integer version) {
+public record VacationInfoUpdateTotalCountRequest(@NotNull Integer id, @NotNull @PositiveOrZero Double totalCount,
+												  @NotNull String type, @NotNull @PositiveOrZero Integer version) {
 
 	public boolean isSameType(String type) {
 		return this.type.equals(type);
